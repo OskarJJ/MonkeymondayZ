fba3f69a90f09313837cbcecd807f7484aa99369
OskarJJ <tox_3_@hotmail.com>
2015-04-10 15:50:45 +0200

    #6-8
    accelorskosmodmameter

--- app/build/generated/source/r/debug/nu/fml/monkeymondayz/R.java
+++ app/build/generated/source/r/debug/nu/fml/monkeymondayz/R.java
@@ -1325,12 +1325,12 @@ containing a value of this type.
         public static final int action_mode_bar=0x7f09002e;
         public static final int action_mode_bar_stub=0x7f09002d;
         public static final int action_mode_close_button=0x7f090020;
-        public static final int action_settings=0x7f090044;
+        public static final int action_settings=0x7f090046;
         public static final int activity_chooser_view_content=0x7f090021;
         public static final int always=0x7f090016;
         public static final int beginning=0x7f090013;
-        public static final int buttonaccel=0x7f090043;
-        public static final int buttongps=0x7f090042;
+        public static final int buttonaccel=0x7f090045;
+        public static final int buttongps=0x7f090044;
         public static final int checkbox=0x7f090029;
         public static final int collapseActionView=0x7f090017;
         public static final int decor_content_parent=0x7f09002f;
@@ -1342,7 +1342,6 @@ containing a value of this type.
         public static final int end=0x7f090014;
         public static final int expand_activities_button=0x7f090022;
         public static final int expanded_menu=0x7f090028;
-        public static final int gogogo=0x7f09003f;
         public static final int home=0x7f090004;
         public static final int homeAsUp=0x7f09000d;
         public static final int icon=0x7f090026;
@@ -1350,7 +1349,7 @@ containing a value of this type.
         public static final int image=0x7f090023;
         public static final int listMode=0x7f090009;
         public static final int list_item=0x7f090025;
-        public static final int main123=0x7f090041;
+        public static final int main123=0x7f090043;
         public static final int middle=0x7f090015;
         public static final int never=0x7f090019;
         public static final int none=0x7f09000e;
@@ -1376,11 +1375,14 @@ containing a value of this type.
         public static final int submit_area=0x7f09003c;
         public static final int tabMode=0x7f09000b;
         public static final int title=0x7f090027;
-        public static final int txtGPS=0x7f090040;
+        public static final int txtGPS=0x7f090042;
         public static final int up=0x7f090008;
         public static final int useLogo=0x7f090012;
         public static final int withText=0x7f09001a;
         public static final int wrap_content=0x7f09001d;
+        public static final int xnumber=0x7f09003f;
+        public static final int ynumber=0x7f090040;
+        public static final int znumber=0x7f090041;
     }
     public static final class integer {
         public static final int abc_config_activityDefaultDur=0x7f0a0000;
--- app/src/main/java/nu/fml/monkeymondayz/ACCELActivity.java
+++ app/src/main/java/nu/fml/monkeymondayz/ACCELActivity.java
@@ -1,21 +1,47 @@
 package nu.fml.monkeymondayz;
 
 import android.app.Activity;
+import android.content.Context;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
 
 
 public class ACCELActivity extends Activity implements SensorEventListener{
+    private SensorManager senSensorManager;
+    private Sensor senAccelerometer;
+
+    private TextView text1;
+    private TextView text2;
+    private TextView text3;
+
+    private long lastUpdate = 0;
+    private float last_x, last_y, last_z;
+    private static final int SHAKE_THRESHOLD = 600;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_accel);
+
+        senSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        senAccelerometer = senSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        senSensorManager.registerListener(this, senAccelerometer, SensorManager.SENSOR_DELAY_NORMAL);
+        setUpViews();
+
+    }
+    private void setUpViews() {
+        text1 = (TextView)findViewById(R.id.xnumber);
+        text2 = (TextView)findViewById(R.id.ynumber);
+        text3 = (TextView)findViewById(R.id.znumber);
     }
 
 
@@ -40,10 +66,43 @@ public class ACCELActivity extends Activity implements SensorEventListener{
 
         return super.onOptionsItemSelected(item);
     }
-
+    protected void onPause(){
+        super.onPause();
+        senSensorManager.unregisterListener(this);
+    }
+    protected void onResume(){
+        super.onResume();
+        senSensorManager.registerListener(this, senAccelerometer, SensorManager.SENSOR_DELAY_NORMAL);
+    }
     @Override
-    public void onSensorChanged(SensorEvent event) {
+    public void onSensorChanged(SensorEvent sensorEvent) {
+        Sensor mySensor = sensorEvent.sensor;
+        if (mySensor.getType() == Sensor.TYPE_ACCELEROMETER){
+            float x = sensorEvent.values[0];
+            float y = sensorEvent.values[1];
+            float z = sensorEvent.values[2];
 
+            long curTime = System.currentTimeMillis();
+
+            if((curTime - lastUpdate) > 100){
+                long diffTime = (curTime - lastUpdate);
+                lastUpdate = curTime;
+
+                float speed = Math.abs(x + y +z -last_x -last_y - last_z)/diffTime * 10000;
+
+                if(speed > SHAKE_THRESHOLD){
+
+                    text1.setText("X: " + String.valueOf(Math.round(x)));
+                    text2.setText("Y: " + String.valueOf(Math.round(y)));
+                    text3.setText("Z: " + String.valueOf(Math.round(z)));
+                }
+                last_x = x;
+                last_y = y;
+                last_z = z;
+
+            }
+
+        }
     }
 
     @Override
